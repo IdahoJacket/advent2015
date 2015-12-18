c001197019da7ef1f2870bdca8ca37319a498ec2
Thomas Anderson <IdahoJacket@users.noreply.github.com>
2015-12-18 21:18:55 +0900

    added day 13

---
+++ 13a.py
@@ -0,0 +1,34 @@
+import itertools
+
+def score( r, x ):
+  s = 0
+  for p in xrange(0,len(x)):
+    s += r[x[p]][x[(p+1)%len(x)]]
+    s += r[x[p]][x[p-1]]
+  return s
+people = set()
+r = {}
+while True:
+  try:
+   l = raw_input()
+  except EOFError:
+    break
+  l = l.split()
+
+  ( p1, a, p2 ) = ( l[0], int(l[3]), l[10] )
+  p2 = p2.strip('.')
+  if l[2] == 'lose':
+    a = -a
+  people.add( p1 )
+  if p1 not in r:
+    r[p1] = {}
+  r[p1][p2] = a
+
+p = itertools.permutations(people)
+best = -99999999
+for x in p:
+  s = score( r, x )
+  if s > best:
+    best = s
+
+print best
